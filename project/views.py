caea06bd7a6e5976a63b48759df52e350d1cf410
Ima Junryou <imajunryou@gmail.com>
2017-09-07 20:13:14 -0600

    Delete Entry view; tasks template

--- project/views.py
+++ project/views.py
@@ -108,4 +108,9 @@ def complete(task_id):
 @app.route("/delete/<int:task_id>/")
 @login_required
 def delete_entry(task_id):
-    pass
+    g.db = connect_db()
+    g.db.execute("delete from tasks where task_id=" + str(task_id))
+    g.db.commit()
+    g.db.close()
+    flash("The task was deleted")
+    return redirect(url_for("tasks"))
